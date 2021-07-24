0648964c92bc3fe67786fb0a35af615b88fab93a
Quốc Nhật <43816037+QNhatCNTT@users.noreply.github.com>
2021-07-25 00:04:24 +0700

    config routes controller views

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,11 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 let initWebRoutes = (app) => {
-    router.get("/", (req, res) => {
-        return res.send('Hello world')
-    })
+    router.get("/", homeController.getHomePage);
+
+    return app.use('/', router);
 };
 
 module.exports = initWebRoutes;
