d2c89464f201c6bbf413062b6dfde9f2877dbbdf
Quốc Nhật <43816037+QNhatCNTT@users.noreply.github.com>
2021-07-25 00:09:18 +0700

    get post webhook

--- src/routes/web.js
+++ src/routes/web.js
@@ -4,7 +4,8 @@ import homeController from "../controllers/homeController";
 let router = express.Router();
 let initWebRoutes = (app) => {
     router.get("/", homeController.getHomePage);
-
+    router.post('/webhook', homeController.postWebhook);
+    router.get('/webhook', homeController.getWebhook);
     return app.use('/', router);
 };
 
